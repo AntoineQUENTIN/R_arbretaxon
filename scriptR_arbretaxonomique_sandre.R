a614b83f577f23802d196ee4e70b16a332a4449f
AntoineQUENTIN <quentin.antoine@gmail.com>
2017-03-27 15:58:36 +0200

    Update scriptR_arbretaxonomique_sandre.R

--- scriptR_arbretaxonomique_sandre.R
+++ scriptR_arbretaxonomique_sandre.R
@@ -1,10 +1,18 @@
+#Package nécessaire:
+if(!require(data.tree)){  install.packages("data.tree");  require(data.tree)} 
+
+#Arbre du sandre
+#http://mdm.sandre.eaufrance.fr/?q=mdm_sandre/treetax/htmlbranchearbre/0
+#Dictionnaire des données
+#http://sandre.eaufrance.fr/ftp/documents/fr/ddd/tax/2002-1/sandre_dictionnaire_TAX_2002-1.pdf
+
 # 1. Lecture des données taxonomiques sur le site du SANDRE
 csv.url <- "http://services.sandre.eaufrance.fr/References/1.0.0/References.php?CdReferentiel=TAX&Filter=++++%3CFilter%3E++++++%3CStatut%3E1%3C%2FStatut%3E++++++%3CStatut%3E0%3C%2FStatut%3E++++%3C%2FFilter%3E&request=getReferenceElements&version=1.0.0&service=References&outputSchema=http%3A%2F%2Fxml.sandre.eaufrance.fr%2Fscenario%2FReferences%2F1&outputFormat=sandre%2Fsimplexml"
 datataxon <-read.csv(csv.url,sep=";",header = T,encoding = "UTF-8")
 
 # 2. Liste des codes Taxons des espèces à dessiner
-tmpCdTAxon <- data.frame(CdTAxon= c("866","1547","2050", "2071" ,"2110" ,"2113", "2117","2125", "2133" ,"2137", "2167", "2177", "2193" ,"2203"))
-
+#tmpCdTAxon <- data.frame(CdTAxon= c("866","1547","2010","1245","865"))
+tmpCdTAxon <- data.frame(CdTAxon= c("2038", "2050", "866","2071" ,"2110" ,"2113", "2117","2125", "2133" ,"2137", "2167", "2177", "2193" ,"2203"))
 # 3. Recherche en cascade des codes Taxon Parents
 arbretaxonmerge <- data.frame()
 for (j in 1:length(tmpCdTAxon$CdTAxon)){
@@ -26,10 +34,10 @@ for (j in 1:length(tmpCdTAxon$CdTAxon)){
 
 # 4. Restitution des branches taxons par espèce
 arbretaxonmerge <- subset(arbretaxonmerge,V3<16 )
-A <- matrix(NA,nrow=length(arbretaxonmerge$V1),ncol=length(unique(arbretaxonmerge$V3)))
+A <- matrix(NA,nrow=length(arbretaxonmerge$V1),ncol=9)
 A <- data.frame(A)
 A[1:length(unique(arbretaxonmerge$V1)),1] <- as.character(unique(arbretaxonmerge$V1))
-colnames(A)<- sort(unique(arbretaxonmerge$V3))
+
 
 for (j in 1:length(tmpCdTAxon$CdTAxon)){
   arbretaxon <- data.frame()
@@ -45,19 +53,19 @@ for (j in 1:length(tmpCdTAxon$CdTAxon)){
   arbretaxon <- subset(arbretaxon,V3<16 )
   A[which(A[,1] == arbretaxon$V1[1]),1:length(arbretaxon$V1)] <- arbretaxon$V1
 }
-
-A <- na.omit(A[,1:9])
-
+#On ne garde que ceux dont la chaine est complète
+A <- na.omit(A)
+colnames(A)<- c("Espece","Genre","Famille","Ordre","sous-Classe","Classe","Super-Classe","Sous-Embranchement","Embranchement")
 # 5.1 Définition du niveau des noeuds
-A$pathString <- paste(A$`13`,A$`4`,A$`3`,A$`2`,A$`1`,A$`0`,
+A$pathString <- paste(A$Embranchement,A$Classe,A$Ordre,A$Famille,A$Genre,A$Espece,
                     sep = "/")
 # 5.2 Noeuds de l'arbre					
 Atree <- as.Node(A)
 
 # 6 . Graphique
 plot(as.dendrogram(Atree), center = T, horiz = T,xlim=c(100,-50),xaxt ='n',yaxt = 'n')
-text(x=90,y=length(A$`0`)+1,label="Embranchement",font = 2)
-text(x=60,y=length(A$`0`)+1,label="Classe",font = 2)
-text(x=40,y=length(A$`0`)+1,label="Famille",font = 2)
-text(x=20,y=length(A$`0`)+1,label="Genre",font = 2)
-text(x=0,y=length(A$`0`)+1,label="Espece",font = 2)
+text(x=90,y=length(A$Embranchement)+1,label="Embranchement",font = 2)
+text(x=60,y=length(A$Embranchement)+1,label="Classe",font = 2)
+text(x=40,y=length(A$Embranchement)+1,label="Famille",font = 2)
+text(x=20,y=length(A$Embranchement)+1,label="Genre",font = 2)
+text(x=0,y=length(A$Embranchement)+1,label="Espece",font = 2)
